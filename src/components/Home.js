6887bd7d5047935c031ee5e326df3c53ed51caba
ayan4m1 <andrew@bulletlogic.com>
2022-04-01 01:29:35 -0400

    add donate link

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,10 @@ export default function Home() {
       <Card body bg="light">
         <Card.Title>Welcome to Filacalc</Card.Title>
         <Card.Text>We provide utilities to help with 3D printing.</Card.Text>
+        <Card.Text>
+          If these utilities help you, consider a{' '}
+          <a href="https://paypal.me/ayan4m1">donation</a> to the author.
+        </Card.Text>
       </Card>
     </Layout>
   );
