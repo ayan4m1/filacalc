e265f246a59dba3f60f1fc7a4c48ae36e65a2932
ayan4m1 <andrew@bulletlogic.com>
2022-04-05 17:57:02 -0400

    make extruder calibration on-the-fly instead of on-submit

--- src/components/ExtruderCalibration.js
+++ src/components/ExtruderCalibration.js
@@ -1,28 +1,29 @@
 import { useFormik } from 'formik';
-import { Fragment, useCallback, useState } from 'react';
-import { Alert, Button, Form } from 'react-bootstrap';
+import { Fragment } from 'react';
+import { Card, Form } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 
 export default function ExtruderCalibration() {
-  const [newSteps, setNewSteps] = useState(0);
   const initialValues = {
     currentSteps: 100,
     extrudedLength: 100
   };
 
-  const { values, handleChange, handleSubmit } = useFormik({
-    initialValues,
-    onSubmit: useCallback(
-      (vals) => setNewSteps((vals.currentSteps / vals.extrudedLength) * 1e2),
-      [setNewSteps]
-    )
+  const { values, handleChange } = useFormik({
+    initialValues
   });
+
+  let newSteps = 0;
+
+  if (values.extrudedLength > 0) {
+    newSteps = (values.currentSteps / values.extrudedLength) * 1e2;
+  }
 
   return (
     <Fragment>
       <Helmet title="Extruder Calibration" />
       <h1>Extruder Steps Calibration</h1>
-      <Form onSubmit={handleSubmit}>
+      <Form>
         <Form.Group>
           <Form.Label>Current Steps per mm</Form.Label>
           <Form.Control
@@ -41,17 +42,13 @@ export default function ExtruderCalibration() {
             onChange={handleChange}
           />
         </Form.Group>
-        <Form.Group className="mt-2">
-          <Button variant="primary" type="submit">
-            Calculate
-          </Button>
-        </Form.Group>
       </Form>
-      {newSteps > 0 && (
-        <Alert className="mt-4" variant="success">
-          Corrected steps per mm: {newSteps.toFixed(2)}
-        </Alert>
-      )}
+      <Card body className="my-4">
+        <Card.Title>Results</Card.Title>
+        <p>
+          The corrected value is <strong>{newSteps.toFixed(2)} steps/mm</strong>
+        </p>
+      </Card>
     </Fragment>
   );
 }
