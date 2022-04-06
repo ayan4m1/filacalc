65c2f4c6ca709d45cab41b4df9af3f52ca1a9675
ayan4m1 <andrew@bulletlogic.com>
2022-04-05 22:02:16 -0400

    use code splitting for different pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,41 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from 'components/Layout';
-import Home from 'components/Home';
-import FilamentUsage from 'components/FilamentUsage';
-import SpoolUsage from 'components/SpoolUsage';
-import ExtruderCalibration from 'components/ExtruderCalibration';
-import VolumetricFlow from 'components/VolumetricFlow';
-import ZAxisCalibration from 'components/ZAxisCalibration';
+import SuspenseFallback from 'components/SuspenseFallback';
+
+const Home = lazy(() =>
+  import(/* webpackChunkName: "home" */ 'components/Home')
+);
+const FilamentUsage = lazy(() =>
+  import(/* webpackChunkName: "filament" */ 'components/FilamentUsage')
+);
+const SpoolUsage = lazy(() =>
+  import(/* webpackChunkName: "spool" */ 'components/SpoolUsage')
+);
+const ExtruderCalibration = lazy(() =>
+  import(/* webpackChunkName: "extruder" */ 'components/ExtruderCalibration')
+);
+const VolumetricFlow = lazy(() =>
+  import(/* webpackChunkName: "flow" */ 'components/VolumetricFlow')
+);
+const ZAxisCalibration = lazy(() =>
+  import(/* webpackChunkName: "zaxis" */ 'components/ZAxisCalibration')
+);
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/filament" element={<FilamentUsage />} />
-        <Route path="/spool" element={<SpoolUsage />} />
-        <Route path="/extruder" element={<ExtruderCalibration />} />
-        <Route path="/flow" element={<VolumetricFlow />} />
-        <Route path="/z-axis" element={<ZAxisCalibration />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<SuspenseFallback />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/filament" element={<FilamentUsage />} />
+          <Route path="/spool" element={<SpoolUsage />} />
+          <Route path="/extruder" element={<ExtruderCalibration />} />
+          <Route path="/flow" element={<VolumetricFlow />} />
+          <Route path="/z-axis" element={<ZAxisCalibration />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Fragment } from 'react';
 import { Container } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
@@ -5,14 +6,19 @@ import { Outlet } from 'react-router-dom';
 
 import Header from 'components/Header';
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <Fragment>
       <Helmet titleTemplate="Filacalc :: %s" />
       <Header />
       <Container>
         <Outlet />
+        {children}
       </Container>
     </Fragment>
   );
 }
+
+Layout.propTypes = {
+  children: PropTypes.node
+};
