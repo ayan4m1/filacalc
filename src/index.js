38d7be3735286729caf26648af2ea356d16a4d7e
ayan4m1 <andrew@bulletlogic.com>
2022-04-19 04:04:55 -0400

    remove relative import

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 
 import './index.scss';
-import App from './components/App';
+import App from 'components/App';
 
 ReactDOM.render(
   <Router>
