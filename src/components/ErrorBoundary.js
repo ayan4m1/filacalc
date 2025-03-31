3adc68b08ba006652b132ce72daca7341974291e
ayan4m1 <andrew@bulletlogic.com>
2025-03-30 23:14:47 -0400

    improve look of error pages

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Container } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
 import { isRouteErrorResponse, useRouteError } from 'react-router';
 
 export default function ErrorBoundary() {
@@ -8,25 +8,34 @@ export default function ErrorBoundary() {
   if (isRouteErrorResponse(error)) {
     return (
       <Container>
-        <h1>
+        <h1 className="display-1">Uh-oh.</h1>
+        <h1 className="mt-0">
           {error.status} {error.statusText}
         </h1>
-        <p>{error.data}</p>
+        <Card bg="primary" body text="white">
+          {error.data}
+        </Card>
       </Container>
     );
   } else if (error instanceof Error) {
     return (
       <Container>
-        <h1>Error</h1>
-        <p>{error.message}</p>
-        <p>The stack trace is:</p>
-        <pre>{error.stack}</pre>
+        <h1 className="display-1">Uh-oh.</h1>
+        <h1 className="mt-0">An error occurred. Please refresh the page.</h1>
+        <Card bg="primary" body className="my-3" text="white">
+          {error.message}
+        </Card>
+        <h2 className="mt-2">The stack trace (for developers):</h2>
+        <Card bg="info" body className="my-3" text="white">
+          <pre className="my-0">{error.stack}</pre>
+        </Card>
       </Container>
     );
   } else {
     return (
       <Container>
-        <h1>Unknown Error Occurred</h1>
+        <h1 className="display-1">Uh-oh.</h1>
+        <h1>An unknown error occurred. Please refresh the page.</h1>
       </Container>
     );
   }
