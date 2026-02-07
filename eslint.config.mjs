5409f9e081c987055f5ea77b4df12de7e0293a87
ayan4m1 <andrew@bulletlogic.com>
2026-02-07 11:23:03 -0500

    chore: upgrade dependencies and clean up eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,13 @@ import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
 import { flatConfigs as importConfig } from 'eslint-plugin-import-x';
 
-const { configs: reactConfig } = reactPlugin;
-const { configs: hooksConfig } = reactHooksPlugin;
-
 export default [
   js.configs.recommended,
   importConfig.recommended,
   importConfig.react,
-  reactConfig.flat['recommended'],
-  hooksConfig.flat['recommended-latest'],
+  reactPlugin.configs.flat['recommended'],
+  reactHooksPlugin.configs.flat['recommended-latest'],
   {
-    plugins: {
-      react: reactPlugin
-    },
     languageOptions: {
       globals: globals.browser,
       parser: babelParser,
@@ -29,13 +23,7 @@ export default [
     rules: {
       'react/jsx-uses-react': 0,
       'react/jsx-sort-props': 2,
-      'react/react-in-jsx-scope': 0,
-      'import-x/no-unresolved': [
-        'error',
-        {
-          caseSensitive: false
-        }
-      ]
+      'react/react-in-jsx-scope': 0
     },
     settings: {
       react: {
